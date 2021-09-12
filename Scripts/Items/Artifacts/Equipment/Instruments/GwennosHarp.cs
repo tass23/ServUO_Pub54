11a09317f9de6ab97889d85ac0917bea5e948f98
Raist <raist@uoexpanse.com>
2021-09-12 05:43:11 -0400

    Add files via upload

--- Scripts/Items/Artifacts/Equipment/Instruments/GwennosHarp.cs
+++ Scripts/Items/Artifacts/Equipment/Instruments/GwennosHarp.cs
@@ -2,20 +2,18 @@ using System;
 
 namespace Server.Items
 {
-    [Flipable(0x315C, 0x315D)]
-    public class DreadFlute : BaseInstrument
+    public class GwennosHarp : LapHarp
 	{
 		public override bool IsArtifact { get { return true; } }
         [Constructable]
-        public DreadFlute()
-            : base(0x315C, 0x58B, 0x58C)// TODO check sounds
+        public GwennosHarp()
         {
-            this.Weight = 1.0;
-            this.ReplenishesCharges = true;
-            this.Hue = 0x4F2;
+            this.Hue = 0x47E;
+            this.Slayer = SlayerName.Repond;
+            this.Slayer2 = SlayerName.ReptilianDeath;
         }
 
-        public DreadFlute(Serial serial)
+        public GwennosHarp(Serial serial)
             : base(serial)
         {
         }
@@ -24,41 +22,34 @@ namespace Server.Items
         {
             get
             {
-                return 1075089;
+                return 1063480;
             }
-        }// Dread Flute
+        }
         public override int InitMinUses
         {
             get
             {
-                return 700;
+                return 1600;
             }
         }
         public override int InitMaxUses
         {
             get
             {
-                return 700;
-            }
-        }
-        public override TimeSpan ChargeReplenishRate
-        {
-            get
-            {
-                return TimeSpan.FromMinutes(15.0);
+                return 1600;
             }
         }
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
-			
+
             writer.Write((int)0); // version
         }
 
         public override void Deserialize(GenericReader reader)
         {
             base.Deserialize(reader);
-			
+
             int version = reader.ReadInt();
         }
     }
